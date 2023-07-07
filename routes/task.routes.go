46cab040f62f7614c277a2e65e846aec72b0cd84
Victor Caro <victor.caro.rainao@gmail.com>
2023-07-07 18:06:01 -0400

    Querys

--- routes/task.routes.go
+++ routes/task.routes.go
@@ -1,1 +1,26 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/VictorCaro/go-restapi/db"
+	"github.com/VictorCaro/go-restapi/models"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
+}
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
