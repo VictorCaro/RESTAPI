b66365d13e0717c827fa203a6954814cf4158c6d
Victor Caro <victor.caro.rainao@gmail.com>
2023-07-07 14:30:42 -0400

    Post/Get, User

--- main.go
+++ main.go
@@ -15,9 +15,16 @@ func main() {
 
 	db.DB.AutoMigrate(models.Task{})
 	db.DB.AutoMigrate(models.User{})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
+
+	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	r.HandleFunc("/users", routes.DeleteUserHandler).Methods("DELETE")
+
 	http.ListenAndServe(":3000", r)
 
 }
--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,1 +1,37 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/VictorCaro/go-restapi/db"
+	"github.com/VictorCaro/go-restapi/models"
+)
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get user"))
+}
+
+func PostUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&user)
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete"))
+}
