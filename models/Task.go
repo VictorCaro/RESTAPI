22da421fc8906f1ff3e550444837bfa977cbf0c3
Victor Caro <victor.caro.rainao@gmail.com>
2023-07-07 18:47:12 -0400

    finals, Tasks

--- models/Task.go
+++ models/Task.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Task struct {
 	gorm.Model
 
-	Tittle      string `gorm:"type:varchar(100);not null;unique_index`
-	Description string
-	Done        bool `gorm:"default:false"`
-	UserID      uint
+	Tittle      string `gorm:"type:varchar(100);not null;unique_index" json:"tittle"`
+	Description string `json:"description"`
+	Done        bool   `gorm:"default:false" json:"done"`
+	UserID      uint   `json:"user_id"`
 }
--- routes/task.routes.go
+++ routes/task.routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/VictorCaro/go-restapi/db"
 	"github.com/VictorCaro/go-restapi/models"
+	"github.com/gorilla/mux"
 )
 
 func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,13 +15,50 @@ func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks models.Task
+	json.NewDecoder(r.Body).Decode(&tasks)
+	createdTask := db.DB.Create(&tasks)
+	err := createdTask.Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	json.NewEncoder(w).Encode(tasks)
 
 }
 
 func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
 
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) //404
+		w.Write([]byte("Task not found"))
+		return
+	}
+	json.NewEncoder(w).Encode(task)
 }
 
-func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+func DeleteTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) //404
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	db.DB.Unscoped().Delete(&task)
+	w.WriteHeader(http.StatusNoContent) // 204
+
+	json.NewEncoder(w).Encode(task)
 
 }
